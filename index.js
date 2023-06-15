24fb75dee6e90112b8bf6ffe6e5a799d33afd9dd
suryakommanapalli <suryakommanapalli80@gmail.comgit config --global user.email suryakommanapalli80@gmail.com>
2023-06-15 11:59:20 +0000

    superOverLeagueApp added successfully

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+<div className = "super-over-league-container">
+<h1 className= "heading">Super Over League</h1>
+    <div className = "teams-container">
+        <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="logo-image"</img>
+    </div>
+     <div className = "imageContainer">
+        <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="logo-image"</img>
+    </div>
+</div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
